0e7b53e428c1cd7aeacb7e54b2fca2912a586089
Pavel Vishnyakov <pvish@outlook.com>
2016-11-24 11:26:59 +0300

    Last commit

--- 166.py
+++ 166.py
@@ -1,13 +1,17 @@
 import socket
+import os
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
         conn, addr = s.accept()
-        data = conn.recv(1024)
-        if data.decode('utf8').strip() == 'close':
-            conn.close()
-        else:
-            conn.send(data)
-            conn.close()
-
+        child_pid = os.fork()
+        if child_pid == 0:
+            data = conn.recv(1024)
+            if data.decode('utf8').strip() == 'close':
+                conn.close()
+            else:
+                conn.send(data)
+                conn.close()
+s.close()
